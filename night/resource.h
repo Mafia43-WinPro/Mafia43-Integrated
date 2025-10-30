dad01b858a055bb29a2e90b5fa55b13176c27424
wib2 <zjavbxjxp@gmail.com>
2025-10-30 10:05:06 +0900

    핸들러Update README.md

---
+++ night/resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// night.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_NIGHT_DIALOG                102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDC_LBL_ROLE                    1001
+#define IDC_LBL_TIMER                   1002
+#define IDC_LIST_PLAYERS                1004
+#define IDC_CMB_ACTION                  1005
+#define IDC_BTN_CONFIRM                 1006
+#define IDC_BTN_SKIP                    1007
+#define IDC_LBL_PREVIEW                 1008
+#define IDC_RE_CHATVIEW                 1009
+#define IDC_EDT_CHAT                    1010
+#define IDC_BTN_SEND                    1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
